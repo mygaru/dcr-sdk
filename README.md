fe06a8b0f686cd768b2715dea5a38ec1e07e2c05
madalv <vlada.magal@isa.utm.md>
2024-09-25 21:45:52 +0300

    modified README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # dcr-sdk
+
+mygaru-client allows checking if UID is in a segment or to scan a list of them and see the overlap % between your list and the segment.
+
+### Scanning
+Note: the minimum limit of UIDs to send is 100.
+
+```go
+// init client with your partnerID
+mygClient := Init(241, 30*time.Second)
+
+segmentId := uint32(1000002)
+uids := make([]string, 100)
+for i := 0; i < 100; i++ {
+    uids[i] = fmt.Sprintf("acefwevreger%d", i)
+}
+
+// in case your uid list is []string
+inter1, err := mygClient.Scan(uids, segmentId)
+
+uidsBytes := []byte(strings.Join(uids, ",\n"))
+// in case your uid list is already encoded
+inter2, err := mygClient.ScanReader(bytes.NewBuffer(uidsBytes), segmentId)
+
+// in case you're reading the uids from a file, network response, etc.
+inter3, err := mygClient.ScanBytes(uidsBytes, segmentId)
+```
--- mygaru-client/mygaru_client_test.go
+++ mygaru-client/mygaru_client_test.go
@@ -1,8 +1,8 @@
-package dcr_sdk
+package mygaru_client
 
 import (
 	"bytes"
-	myg "github.com/mygaru/dcr-sdk/mygaru-client"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
@@ -10,25 +10,30 @@ import (
 )
 
 func TestSDK_Scan(t *testing.T) {
-	mygClient := myg.Init(241, 30*time.Second)
+	mygClient := Init(241, 30*time.Second)
 	segmentId := uint32(1000002)
 
-	uids := []string{"acefwevreger9", "acefwevreger1", "acefwevreger0", "d", "e", "f", "j", "h", "i", "h2", "4"}
+	uids := make([]string, 100)
+	for i := 0; i < 100; i++ {
+		uids[i] = fmt.Sprintf("acefwevreger%d", i)
+	}
 	inter1, err := mygClient.Scan(uids, segmentId)
 	assert.Nil(t, err)
 
 	uidsBytes := []byte(strings.Join(uids, ",\n"))
 	inter2, err := mygClient.ScanReader(bytes.NewBuffer(uidsBytes), segmentId)
+	assert.Nil(t, err)
 
 	inter3, err := mygClient.ScanBytes(uidsBytes, segmentId)
+	assert.Nil(t, err)
 
 	assert.True(t, inter1 == inter2 && inter3 == inter1)
 }
 
 func TestSDK_Check_ExternalUID(t *testing.T) {
-	mygClient := myg.Init(241, 30*time.Second)
+	mygClient := Init(241, 30*time.Second)
 
-	ok, err := mygClient.Check("acefwevreger9", 1000002, myg.IdentifierTypeExternal)
+	ok, err := mygClient.Check("acefwevreger9", 1000002, IdentifierTypeExternal)
 	assert.Nil(t, err)
 
 	t.Log(ok)
